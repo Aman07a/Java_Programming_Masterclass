7784a2a62859aea0ac5294d4857ac7ffcd7f7d8c
Aman07a <aman16.aa17@gmail.com>
2023-08-28 16:13:24 +0200

    Fixed: Code Blocks And The If Then Else Control Statement

--- KeywordsAndExpressions/src/Main.java
+++ KeywordsAndExpressions/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         boolean gameOver = true;
-        int score = 8000;
+        int score = 800;
         int levelCompleted = 5;
         int bonus = 100;
 
