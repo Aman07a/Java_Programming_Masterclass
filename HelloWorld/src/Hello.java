75ded0d62b4d0f0ceb9320ea2f9db079001e5a8b
Aman07a <aman16.aa17@gmail.com>
2023-08-28 14:34:17 +0200

    Hello World in IntelliJ

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,5 +1,6 @@
-public class FirstClass {
+public class Hello {
     public static void main(String[] args) {
         System.out.print("Hello, Aman");
+        System.out.print("Hello World");
     }
 }
