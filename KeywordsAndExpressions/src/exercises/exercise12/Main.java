e32ae54b4fba9c0f7df8caf9a16302d3e243c947
Aman07a <aman16.aa17@gmail.com>
2023-08-29 13:03:54 +0200

    Playing Cat - Solution

---
+++ KeywordsAndExpressions/src/exercises/exercise12/Main.java
@@ -0,0 +1,10 @@
+package exercises.exercise12;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(PlayingCat.isCatPlaying(true, 10));  // Output: false
+        System.out.println(PlayingCat.isCatPlaying(false, 36)); // Output: false
+        System.out.println(PlayingCat.isCatPlaying(false, 35)); // Output: true
+    }
+}
+
