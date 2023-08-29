57af85418dc7601264a0a020f5eade16a3fa4c7f
Aman07a <aman16.aa17@gmail.com>
2023-08-29 15:30:23 +0200

    Number In Word - Solution

---
+++ ChapterSixExercises/src/exercise13/Main.java
@@ -0,0 +1,11 @@
+package exercise13;
+
+public class Main {
+    public static void main(String[] args) {
+        // Test cases
+        NumberInWord.printNumberInWord(5);   // Output: FIVE
+        NumberInWord.printNumberInWord(0);   // Output: ZERO
+        NumberInWord.printNumberInWord(11);  // Output: OTHER
+        NumberInWord.printNumberInWord(-3);  // Output: OTHER
+    }
+}
