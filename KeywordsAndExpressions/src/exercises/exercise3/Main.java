4ab593c2619fd4854cf2dc385ba874b4d3492211
Aman07a <aman16.aa17@gmail.com>
2023-08-29 07:37:53 +0200

    MegaBytes Converter - Solution

---
+++ KeywordsAndExpressions/src/exercises/exercise3/Main.java
@@ -0,0 +1,10 @@
+package exercises.exercise3;
+
+public class Main {
+    public static void main(String[] args) {
+        MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
+        MegaBytesConverter.printMegaBytesAndKiloBytes(-1024);
+        MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
+    }
+}
+
