76156274a0e9bd0ff333e896e1c2a3ebf451fdc6
Aman07a <aman16.aa17@gmail.com>
2023-08-29 11:58:54 +0200

    Bonus Challenge Solution

--- SecondsAndMinutesChallenge/src/Main.java
+++ SecondsAndMinutesChallenge/src/Main.java
@@ -2,29 +2,39 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(getDurationString(3945));
+        System.out.println(getDurationString(-3945));
+        System.out.println(getDurationString(-65, 45));
+        System.out.println(getDurationString(65, 145));
         System.out.println(getDurationString(65, 45));
+        System.out.println(getDurationString(3945));
     }
 
     public static String getDurationString(int seconds) {
 
-        // Two steps approach to get hours
-        int minutes = seconds / 60;
-        int hours = minutes / 60;
+        if (seconds < 0) {
+            return "Invalid data for seconds(" + seconds
+                    + "), must be a positive integer value";
+        }
 
-        int remainingMinutes = minutes % 60;
-        int remainingSeconds = seconds % 60;
-
-        return hours + "h " + remainingMinutes + "m " + remainingSeconds + "s";
+        return getDurationString(seconds / 60, seconds % 60);
     }
 
     public static String getDurationString(int minutes, int seconds) {
 
+        if (minutes < 0) {
+            return "Invalid data for minutes(" + minutes
+                    + "), must be a positive integer value";
+        }
+
+        if (seconds <= 0 || seconds >= 59) {
+            return "Invalid data for seconds(" + seconds
+                    + "), must be between 0 and 59";
+        }
+
         int hours = minutes / 60;
 
         int remainingMinutes = minutes % 60;
-        int remainingSeconds = seconds % 60;
 
-        return hours + "h " + remainingMinutes + "m " + remainingSeconds + "s";
+        return hours + "h " + remainingMinutes + "m " + seconds + "s";
     }
 }
