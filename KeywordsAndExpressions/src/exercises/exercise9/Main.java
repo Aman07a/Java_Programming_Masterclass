d102487b167f8498cad1f9ff30057f4ff3bfe020
Aman07a <aman16.aa17@gmail.com>
2023-08-29 12:35:28 +0200

    Area Calculator - Solution

---
+++ KeywordsAndExpressions/src/exercises/exercise9/Main.java
@@ -0,0 +1,11 @@
+package exercises.exercise9;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(AreaCalculator.area(5.0));          // Output: 78.53975
+        System.out.println(AreaCalculator.area(-1.0));         // Output: -1.0
+        System.out.println(AreaCalculator.area(5.0, 4.0));     // Output: 20.0
+        System.out.println(AreaCalculator.area(-1.0, 4.0));    // Output: -1.0
+    }
+}
+
