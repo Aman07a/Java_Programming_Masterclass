1fc72bb1f547922dabdedac439d1d965225252a3
Aman07a <aman16.aa17@gmail.com>
2023-08-30 13:30:33 +0200

    Perfect Number - Solution

---
+++ ChapterSixExercises/src/exercise23/Main.java
@@ -0,0 +1,11 @@
+package exercise23;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(PerfectNumber.isPerfectNumber(6));   // Output: true
+        System.out.println(PerfectNumber.isPerfectNumber(28));  // Output: true
+        System.out.println(PerfectNumber.isPerfectNumber(5));   // Output: false
+        System.out.println(PerfectNumber.isPerfectNumber(-1));  // Output: false
+    }
+}
+
