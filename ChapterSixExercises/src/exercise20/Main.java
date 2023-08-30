b7cd6e11396dddea33b29d58be6f1808c1d3defb
Aman07a <aman16.aa17@gmail.com>
2023-08-30 10:50:55 +0200

    Even Digit Sum - Solution

---
+++ ChapterSixExercises/src/exercise20/Main.java
@@ -0,0 +1,10 @@
+package exercise20;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(EvenDigitSum.getEvenDigitSum(123456789));  // Output: 20
+        System.out.println(EvenDigitSum.getEvenDigitSum(252));        // Output: 4
+        System.out.println(EvenDigitSum.getEvenDigitSum(-22));        // Output: -1
+    }
+}
+
