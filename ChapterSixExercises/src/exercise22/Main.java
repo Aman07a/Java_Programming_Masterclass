285148af66fd2bf634a16b2c98c16ebbad5676a1
Aman07a <aman16.aa17@gmail.com>
2023-08-30 12:57:37 +0200

    Greatest Common Divisor - Solution

---
+++ ChapterSixExercises/src/exercise22/Main.java
@@ -0,0 +1,11 @@
+package exercise22;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(GreatestCommonDivisor.getGreatestCommonDivisor(25, 15));   // Output: 5
+        System.out.println(GreatestCommonDivisor.getGreatestCommonDivisor(12, 30));   // Output: 6
+        System.out.println(GreatestCommonDivisor.getGreatestCommonDivisor(9, 18));    // Output: -1
+        System.out.println(GreatestCommonDivisor.getGreatestCommonDivisor(81, 153));  // Output: 9
+    }
+}
+
