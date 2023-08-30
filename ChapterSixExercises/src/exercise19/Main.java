eec8c3ce641754e2fede6c57a8356a7085e9bfe6
Aman07a <aman16.aa17@gmail.com>
2023-08-30 10:27:41 +0200

    First And Last Digit Sum - Solution

---
+++ ChapterSixExercises/src/exercise19/Main.java
@@ -0,0 +1,12 @@
+package exercise19;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(252));   // Output: 4
+        System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(257));   // Output: 9
+        System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(0));     // Output: 0
+        System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(5));     // Output: 10
+        System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(-10));  // Output: -1
+    }
+}
+
