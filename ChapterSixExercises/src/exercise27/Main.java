fa434309d48d94ef95323850b59a5ba05562ba1e
Aman07a <aman16.aa17@gmail.com>
2023-08-31 08:23:23 +0200

    Diagonal Star - Solution

---
+++ ChapterSixExercises/src/exercise27/Main.java
@@ -0,0 +1,12 @@
+package exercise27;
+
+public class Main {
+    public static void main(String[] args) {
+        DiagonalStar.printSquareStar(5);
+
+        System.out.println(); // Empty line to separate the outputs
+
+        DiagonalStar.printSquareStar(8);
+    }
+}
+
