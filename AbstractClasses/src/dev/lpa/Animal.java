fa2935e3dda7876392cc928d4c90706808f2e3fe
Aman07a <aman16.aa17@gmail.com>
2023-09-07 13:49:51 +0200

    Abstract Classes Part 3

--- AbstractClasses/src/dev/lpa/Animal.java
+++ AbstractClasses/src/dev/lpa/Animal.java
@@ -1,5 +1,21 @@
 package dev.lpa;
 
+abstract class Mammal extends Animal {
+
+    public Mammal(String type, String size, double weight) {
+        super(type, size, weight);
+    }
+
+    @Override
+    public void move(String speed) {
+
+        System.out.print(getExplicitType() + " ");
+        System.out.println(speed.equals("slow") ? "walks" : "runs");
+    }
+
+    public abstract void shedHair();
+}
+
 public abstract class Animal {
 
     protected String type;
@@ -16,5 +32,8 @@ public abstract class Animal {
 
     public abstract void makeNoise();
 
+    public final String getExplicitType() {
+        return getClass().getSimpleName() + " (" + type + ")";
+    }
 }
 
