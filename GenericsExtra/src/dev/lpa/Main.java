18a7b2e6f16a3565ffd63b92142bcbd58d9ceb94
Aman07a <aman16.aa17@gmail.com>
2023-09-12 13:25:04 +0200

    Putting it all together, Final Section Challenge

--- GenericsExtra/src/dev/lpa/Main.java
+++ GenericsExtra/src/dev/lpa/Main.java
@@ -1,96 +1,44 @@
 package dev.lpa;
 
 import dev.lpa.model.LPAStudent;
-import dev.lpa.model.Student;
-import dev.lpa.util.QueryItem;
+import dev.lpa.model.LPAStudentComparator;
 import dev.lpa.util.QueryList;
 
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-
-record Employee(String name) implements QueryItem {
-
-    @Override
-    public boolean matchFieldValue(String fieldName, String value) {
-        return false;
-    }
-}
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int studentCount = 10;
-        List<Student> students = new ArrayList<>();
-        for (int i = 0; i < studentCount; i++) {
-            students.add(new Student());
+        QueryList<LPAStudent> queryList = new QueryList<>();
+        for (int i = 0; i < 25; i++) {
+            queryList.add(new LPAStudent());
         }
-        students.add(new LPAStudent());
-//        printList(students);
-        printMoreLists(students);
 
-        List<LPAStudent> lpaStudents = new ArrayList<>();
-        for (int i = 0; i < studentCount; i++) {
-            lpaStudents.add(new LPAStudent());
-        }
-//        printList(lpaStudents);
-        printMoreLists(lpaStudents);
+        System.out.println("Ordered");
+        queryList.sort(Comparator.naturalOrder());
+        printList(queryList);
 
-        testList(new ArrayList<String>(List.of("Able", "Barry", "Charlie")));
-        testList(new ArrayList<Integer>(List.of(1, 2, 3)));
+        System.out.println("Matches");
+        var matches = queryList
+                .getMatches("PercentComplete", "50")
+                .getMatches("Course", "Python");
 
-        var queryList = new QueryList<>(lpaStudents);
-        var matches = queryList.getMatches(
-                "Course", "Python");
-        printMoreLists(matches);
+        matches.sort(new LPAStudentComparator());
+        printList(matches);
 
-        var students2021 =
-                QueryList.getMatches(students, "YearStarted", "2021");
-        printMoreLists(students2021);
-
-//        QueryList<Employee> employeeList = new QueryList<>();
+        System.out.println("Ordered");
+        matches.sort(null);
+        printList(matches);
 
     }
 
-    public static void printMoreLists(List<? extends Student> students) {
+    public static void printList(List<?> students) {
 
         for (var student : students) {
             System.out.println(student);
         }
-        System.out.println();
     }
-
-    public static void testList(List<?> list) {
-
-        for (var element : list) {
-            if (element instanceof String s) {
-                System.out.println("String: " + s.toUpperCase());
-            } else if (element instanceof Integer i) {
-                System.out.println("Integer: " + i.floatValue());
-            }
-        }
-    }
-
-//    public static void testList(List<String> list) {
-//
-//        for (var element : list) {
-//            System.out.println("String: " + element.toUpperCase());
-//        }
-//    }
-//
-//    public static void testList(List<Integer> list) {
-//
-//        for (var element : list) {
-//            System.out.println("Integer: " + element.floatValue());
-//        }
-//    }
-//
-//    public static <T extends Student> void printList(List<T> students) {
-//
-//        for (var student : students) {
-//            System.out.println(student.getYearStarted() + ": " + student);
-//        }
-//        System.out.println();
-//    }
 }
 
