c8815792cdba749ab510d1bfd586bf8ece453af0
Aman07a <aman16.aa17@gmail.com>
2023-09-12 14:42:34 +0200

    Static Nested Classes

--- NestedClasses/src/dev/lpa/domain/EmployeeComparator.java
+++ NestedClasses/src/dev/lpa/domain/EmployeeComparator.java
@@ -1,6 +1,12 @@
 package dev.lpa.domain;
 
-public class EmployeeComparator {
-    
+import java.util.Comparator;
+
+public class EmployeeComparator<T extends Employee> implements Comparator<Employee> {
+
+    @Override
+    public int compare(Employee o1, Employee o2) {
+        return o1.getName().compareTo(o2.getName());
+    }
 }
 
