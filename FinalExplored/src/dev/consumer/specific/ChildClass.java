dafb0a5cf83ea8ab4d09a73bca33e54414bb34c3
Aman07a <aman16.aa17@gmail.com>
2023-09-19 14:16:02 +0200

    Revisiting the final modifier

--- FinalExplored/src/dev/consumer/specific/ChildClass.java
+++ FinalExplored/src/dev/consumer/specific/ChildClass.java
@@ -1,5 +1,25 @@
-package dev.consumer.specific;
+package consumer.specific;
 
-public class ChildClass {
+import dev.lpa.generic.BaseClass;
+
+public class ChildClass extends BaseClass {
+
+    @Override
+    protected void optionalMethod() {
+
+        System.out.println("[Child:optionalMethod] EXTRA Stuff Here");
+        super.optionalMethod();
+    }
+
+//    @Override
+//    public void recommendedMethod() {
+//
+//        System.out.println("[Child:recommendedMethod]: I'll do things my way");
+//        optionalMethod();
+//    }
+
+    private void mandatoryMethod() {
+        System.out.println("[Child:mandatoryMethod]: My own important stuff");
+    }
 }
 
