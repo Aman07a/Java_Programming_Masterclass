2aaa8d76444a880d3d2c18e3571e9c1bbecfd0d5
Aman07a <aman16.aa17@gmail.com>
2023-09-28 12:55:04 +0200

    Serialization & Change - Part 2

--- DataStreams/src/dev/lpa/Main.java
+++ DataStreams/src/dev/lpa/Main.java
@@ -9,24 +9,52 @@ import java.util.List;
 class Player implements Serializable {
 
     private final static long serialVersionUID = 1L;
+    private final static int version = 2;
     private String name;
-    private int topScore;
+    private long topScore;
     private long bigScore;
+    private final transient long accountId;
     private List<String> collectedWeapons = new LinkedList<>();
 
-    public Player(String name, long topScore, List<String> collectedWeapons) {
+    public Player(long accountId, String name, int topScore, List<String> collectedWeapons) {
+        this.accountId = accountId;
         this.name = name;
-        this.bigScore = topScore;
+        this.topScore = topScore;
         this.collectedWeapons = collectedWeapons;
     }
 
     @Override
     public String toString() {
         return "Player{" +
+                "id=" + accountId + ", " +
                 "name='" + name + '\'' +
-                ", bigScore=" + bigScore +
+                ", topScore=" + topScore +
                 ", collectedWeapons=" + collectedWeapons +
                 '}';
+    }
+
+    @Serial
+    @SuppressWarnings("unchecked")
+    private void readObject(ObjectInputStream stream)
+            throws IOException, ClassNotFoundException {
+//        stream.defaultReadObject();
+//        bigScore = (bigScore == 0) ? 1_000_000_000L : bigScore;
+
+        var serializedVer = stream.readInt();
+        collectedWeapons = (List<String>) stream.readObject();
+        name = stream.readUTF();
+        topScore = (serializedVer == 1) ? stream.readInt() : stream.readLong();
+    }
+
+    @Serial
+    private void writeObject(ObjectOutputStream stream)
+            throws IOException {
+
+        System.out.println("--> Customized Writing");
+        stream.writeInt(version);
+        stream.writeObject(collectedWeapons);
+        stream.writeUTF(name);
+        stream.writeLong(topScore);
     }
 }
 
@@ -38,7 +66,7 @@ public class Main {
 //        writeData(dataFile);
 //        readData(dataFile);
 
-        Player tim = new Player("Tim", 100_000_010,
+        Player tim = new Player(555, "Tim", 100_000_010,
                 List.of("knife", "machete", "pistol"));
         System.out.println(tim);
 
@@ -46,6 +74,14 @@ public class Main {
 //        writeObject(timFile, tim);
         Player reconstitutedTim = readObject(timFile);
         System.out.println(reconstitutedTim);
+
+        Player joe = new Player(556, "Joe", 75,
+                List.of("crossbow", "rifle", "pistol"));
+        Path joeFile = Path.of("joe.dat");
+        writeObject(joeFile, joe);
+        Player reconstitutedJoe = readObject(joeFile);
+        System.out.println(joe);
+        System.out.println(reconstitutedJoe);
     }
 
     private static void writeData(Path dataFile) {
