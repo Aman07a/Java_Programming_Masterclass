0c3db96235db0a3da645b7bab33023438e97a5ea
Aman07a <aman16.aa17@gmail.com>
2023-09-29 11:15:20 +0200

    Interrupt and Join

--- Threads/src/com/timbuchalka/Main.java
+++ Threads/src/com/timbuchalka/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(ANSI_PURPLE + "Hello from the main thread.");
 
-        Thread anotherThread = new AnotherThread();
+        final Thread anotherThread = new AnotherThread();
         anotherThread.setName("== Another Thread ==");
         anotherThread.start();
 
@@ -23,6 +23,12 @@ public class Main {
             @Override
             public void run() {
                 System.out.println(ANSI_RED + "Hello from the anonymous class's implementation of run()");
+                try {
+                    anotherThread.join();
+                    System.out.println(ANSI_RED + "AnotherThread terminated, or timed out, so I'm running again");
+                } catch (InterruptedException e) {
+                    System.out.println(ANSI_RED + "I couldn't wait after all. I was interrupted");
+                }
             }
         });
 
