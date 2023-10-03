45336b494d8289036226a34e8bb079c80cf5e10e
Aman07a <aman16.aa17@gmail.com>
2023-10-03 15:20:50 +0200

    Asserts in Junit

--- Banking/src/com/timbuchalka/BankAccountTest.java
+++ Banking/src/com/timbuchalka/BankAccountTest.java
@@ -5,18 +5,35 @@ import static org.junit.jupiter.api.Assertions.*;
 class BankAccountTest {
 
     @org.junit.jupiter.api.Test
-    void deposit() throws Exception {
+    public void deposit() throws Exception {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        double balance = account.deposit(200.00, true);
+        assertEquals(1200.00, balance, 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void withdraw() throws Exception {
         fail("This test has yet to be implemented");
     }
 
     @org.junit.jupiter.api.Test
-    void withdraw() throws Exception {
-        fail("This test has yet to be implemented");
+    public void getBalance_deposit() throws Exception {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        account.deposit(200.00, true);
+        assertEquals(1200.00, account.getBalance(), 0);
     }
 
     @org.junit.jupiter.api.Test
-    void getBalance() throws Exception {
-        fail("This test has yet to be implemented");
+    public void getBalance_withdraw() throws Exception {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        account.withdraw(200.00, true);
+        assertEquals(800.00, account.getBalance(), 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void isChecking_true() {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        assertTrue(account.isChecking(), "The account is NOT a checking account");
     }
 }
 
