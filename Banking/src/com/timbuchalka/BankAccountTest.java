f200a564085771ac29ed4d7fc6303b23d6e6f72c
Aman07a <aman16.aa17@gmail.com>
2023-10-03 14:25:50 +0200

    Introduction to Unit Testing with JUnit

---
+++ Banking/src/com/timbuchalka/BankAccountTest.java
@@ -0,0 +1,22 @@
+package com.timbuchalka;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountTest {
+
+    @org.junit.jupiter.api.Test
+    void deposit() throws Exception {
+        fail("This test has yet to be implemented");
+    }
+
+    @org.junit.jupiter.api.Test
+    void withdraw() throws Exception {
+        fail("This test has yet to be implemented");
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBalance() throws Exception {
+        fail("This test has yet to be implemented");
+    }
+}
+
