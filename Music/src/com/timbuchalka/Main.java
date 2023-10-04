5d046f218572f69868b80eefa95bc6f324e2da70
Aman07a <aman16.aa17@gmail.com>
2023-10-04 15:13:13 +0200

    Write Java Query for Artists

--- Music/src/com/timbuchalka/Main.java
+++ Music/src/com/timbuchalka/Main.java
@@ -1,18 +1,27 @@
 package com.timbuchalka;
 
+import com.timbuchalka.model.Artist;
 import com.timbuchalka.model.Datasource;
 
-import java.sql.Connection;
+import java.util.List;
 
 public class Main {
-
-    private Connection conn;
 
     public static void main(String[] args) {
         Datasource datasource = new Datasource();
         if (!datasource.open()) {
             System.out.println("Can't open datasource");
             return;
+        }
+
+        List<Artist> artists = datasource.queryArtists();
+        if (artists == null) {
+            System.out.println("No artists!");
+            return;
+        }
+
+        for (Artist artist : artists) {
+            System.out.println("ID = " + artist.getId() + ", Name = " + artist.getName());
         }
 
         datasource.close();
