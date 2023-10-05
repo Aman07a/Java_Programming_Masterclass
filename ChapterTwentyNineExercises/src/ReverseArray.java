5df6bec31228a25eb2eb037f1a51affe44697c34
Aman07a <aman16.aa17@gmail.com>
2023-10-05 13:35:50 +0200

    Solution: Reverse Array

---
+++ ChapterTwentyNineExercises/src/ReverseArray.java
@@ -0,0 +1,29 @@
+public class ReverseArray {
+    private static void reverse(int[] arr) {
+        System.out.print("Array = [");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+
+        int temp;
+        for (int i = 0; i < arr.length / 2; i++) {
+            temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
+
+        System.out.print("Reversed array = [");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+}
+
