115e1b22a4ab96eefb3d6098738c9cb648b88880
Aman07a <aman16.aa17@gmail.com>
2023-10-06 10:20:33 +0200

    Logical OR Operator

--- Old_Content/Operators/src/academy/learnprogramming/Main.java
+++ Old_Content/Operators/src/academy/learnprogramming/Main.java
@@ -55,9 +55,23 @@ public class Main {
             System.out.println("You got the high score!");
         }
 
-        int secondTopScore = 81;
-        if (topScore > secondTopScore && topScore < 100) {
+        int secondTopScore = 95;
+        if ((topScore > secondTopScore) && (topScore < 100)) {
             System.out.println("Greater than second top score and less than 100");
+        }
+
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
+
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is true");
+        }
+
+        boolean isCar = false;
+        if (!isCar) {
+            System.out.println("This is not supposed to happen");
         }
     }
 }
