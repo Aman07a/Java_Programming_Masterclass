1bfa73a61b0196e8d6d39c42d079f07fe0ae5b58
Aman07a <aman16.aa17@gmail.com>
2023-10-11 08:28:55 +0200

    OOP Challenge - Solution

--- Old_Content/OOPTest/src/com/timbuchalka/Main.java
+++ Old_Content/OOPTest/src/com/timbuchalka/Main.java
@@ -2,7 +2,7 @@ package com.timbuchalka;
 
 public class Main {
 
-    public static void main(String[] args) {]
+    public static void main(String[] args) {
 
         // The purpose of the application is to help a ficticious company called Bills Burgers to manage
         // their process of selling hamburgers.
@@ -39,6 +39,15 @@ public class Main {
         hamburger.addHamburgerAddition2("Lettuce", 0.75);
         hamburger.addHamburgerAddition3("Cheese", 1.13);
         System.out.println("Total Burger price is " + hamburger.itemizeHamburger());
+
+        HealthyBurger healthyBurger = new HealthyBurger("Bacon", 5.67);
+        healthyBurger.addHamburgerAddition1("Egg", 5.43);
+        healthyBurger.addHealthAddition1("Lentils", 3.41);
+        System.out.println("Total Healthy Burger price is  " + healthyBurger.itemizeHamburger());
+
+        DeluxeBurger db = new DeluxeBurger();
+        db.addHamburgerAddition3("Should not do this", 50.53);
+        db.itemizeHamburger();
     }
 }
 
