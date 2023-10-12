f033909c6fd84db582127ecf5a745c0acc176c80
Aman07a <aman16.aa17@gmail.com>
2023-10-12 18:22:03 +0200

    Our Generics Class

--- Old_Content/Generics/src/com/timbuchalka/Main.java
+++ Old_Content/Generics/src/com/timbuchalka/Main.java
@@ -1,25 +1,18 @@
 package com.timbuchalka;
 
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Integer> items = new ArrayList<>();
-        items.add(1);
-        items.add(2);
-        items.add(3);
-//        items.add("tim");
-        items.add(4);
-        items.add(5);
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
 
-        printDoubled(items);
-    }
+        Team adelaideCrows = new Team("Adelaide Crows");
+        adelaideCrows.addPlayer(joe);
+        adelaideCrows.addPlayer(pat);
+        adelaideCrows.addPlayer(beckham);
 
-    private static void printDoubled(ArrayList<Integer> n) {
-        for (int i : n) {
-            System.out.println(i * 2);
-        }
+        System.out.println(adelaideCrows.numPlayers());
     }
 }
 
