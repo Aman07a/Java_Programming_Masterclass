48ceba175cb0b3626b18007761c4fa5e099fffb4
Aman07a <aman16.aa17@gmail.com>
2023-10-13 09:20:46 +0200

    Streams - Intermediate and Terminal Operations

--- Old_Content/Streams/src/com/timbuchalka/Main.java
+++ Old_Content/Streams/src/com/timbuchalka/Main.java
@@ -3,6 +3,7 @@ package com.timbuchalka;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -33,6 +34,15 @@ public class Main {
                 .filter(s -> s.startsWith("G"))
                 .sorted()
                 .forEach(System.out::println);
+
+        Stream<String> ioNumberStream = Stream.of("I26", "I17", "I29", "O71");
+        Stream<String> inNumberStream = Stream.of("N40", "N36", "I26", "I17", "I29", "O71");
+        Stream<String> concatStream = Stream.concat(ioNumberStream, inNumberStream);
+        System.out.println("-----------------------");
+        System.out.println(concatStream
+                .distinct()
+                .peek(System.out::println)
+                .count());
     }
 }
 
